645bc4fcef22c0aff66b7f748afded91f2b3f15c
Genís Bosch <33487911+TwoPii@users.noreply.github.com>
2017-11-08 14:55:13 +0100

    Add files via upload

--- sys.c
+++ sys.c
@@ -51,13 +51,33 @@ int sys_fork()
 		son_union->stack[i] = (union task_union *)current()->stack[i];
 	}
 	allocate_DIR(s);
-	//inherit user data -> page table child initialized get_PT, page table system code and data can be a copy of
-        // the page(shared), page table user data and stack point new allocated pages
-	// copy user data and stack from parent to child (set ss pag and del ss pag, copy data and stack, free temporal entriesç
-	//stack with content that emulates result of a call to task_switch.
-	//insert process to ready queue
-
+	for(int i = 0; i < NUM_PAG_DATA; ++i){
+		unsigned int a[i] = alloc_frames()
+		if(a[i] == -1) return -103; /*EOFM*/
+	}
+	page_table_entry * PT = get_PT(s);
+	for(int i = 0; i < 255; ++i){ /*System code and data*/
+		set_ss_pag(PT, i, get_frame(get_PT(current(), i)));	
+	}
+	for(int i = USER_FIRST_PAGE; i < USER_FIRST_PAGE + NUM_PAG_CODE; ++i){ /*User code(shared)*/
+		set_ss_pag(PT, i, get_frame(get_PT(current(), i)));
+	}
+	for(int i = 255 + NUM_PAG_CODE; i < 255 + NUM_PAG_CODE + NUM_PAG_DATA; ++i){/*PH frames for user data*/
+		set_ss_pag(PT, i, a[i]);
+	}
+	for(int i =  0; i < NUM_PAG_DATA; ++i){/*Use 1 temporarl entry of father to copy user data*/
+		set_ss_pag(get_PT(current()), 255+ NUM_PAG_CODE + NUM_PAG_DATA + 1, get_frame(PT, 255 + NUM_PAG_CODE + i));
+		copy_data(L_USER_START+ NUM_PAG_CODE * PAGE_SIZE,L_USER_START+ NUM_PAG_CODE * PAGE_SIZE + NUM_PAG_CODE* PAGE_SIZE, PAGE_SIZE);
+		del_ss_pag(get_PT(current()), 255+ NUM_PAG_CODE + NUM_PAG_DATA + 1);	
+	}
+	set_cr3(get_DIR(current());/*flush TLB*/
+	*(s->ebp_ret) = &ret_from_fork/* set ret_from_fork on ebp*/
+	*((s->ebp_ret) - 4) = 0;/*set 0 to get a return*/
+	list_add(&s->list, &readyqueue);/*add to readyqueue*/
 	return son_union->task.PID;
+}
+int ret_from_fork(){
+	return 0;
 }
 
 void sys_exit(){  
